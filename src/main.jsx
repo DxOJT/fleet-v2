6fe63d0f05e41435d77f6fbd63fb34f754f1fef6
Ryan Joshua Charita <ryanjoshuacharita@Ryans-Mac-mini.local>
2023-03-02 16:38:13 +0800

    Add apollo client

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 import "./main.css";
-import router from './routes'
+import router from "./routes";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-
+const client = new ApolloClient({
+  uri: "https://snapmed.hasura.app/v1/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <ChakraProvider>
+    <ChakraProvider>
+      <ApolloProvider client={client}>
         <RouterProvider router={router} />
-      </ChakraProvider>
+      </ApolloProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );

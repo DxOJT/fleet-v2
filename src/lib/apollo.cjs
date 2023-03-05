c6e15d6bbea273b670936eb7a195b87d1ad1b95e
Ryan Joshua Charita <rjcharita23@gmail.com>
2023-03-05 21:56:57 +0800

    add apollo client

---
+++ src/lib/apollo.cjs
@@ -0,0 +1,74 @@
+import { split, HttpLink, ApolloClient, InMemoryCache } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { createClient } from 'graphql-ws';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { setContext } from "@apollo/client/link/context";
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem("Token");
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        },
+    };
+});
+
+const httpLink = new HttpLink({
+    uri: process.env.HASURA_HTTP_URL,
+});
+
+const wsLink = new GraphQLWsLink(createClient({
+    url: process.env.HASURA_WS_URL,
+    on: {
+        connected: () => console.log("Connected client!"),
+        closed: () => console.log("Closed ws-connection!"),
+    },
+    reconnect: true,
+    connectionParams: () => {
+        // Note: getSession() is a placeholder function created by you
+        const session = localStorage.getItem("Token");
+        if (!session) {
+            return {};
+        }
+        return {
+            Authorization: `Bearer ${session}`,
+        };
+    },
+
+}));
+// const wsLink = new WebSocketLink({
+//     uri: 'ws://3.1.204.87:8080/v1/graphql',
+//     options: {
+//         reconnect: true,
+//         connectionParams: {
+//             headers: {
+//                 "content-type": "application/json",
+//                 Authorization: `Bearer ${localStorage.getItem("Token")}`,
+//             }
+//         },
+//     },
+// });
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === "OperationDefinition" &&
+            definition.operation === "subscription"
+        );
+    },
+    wsLink,
+    httpLink
+);
+
+export default new ApolloClient({
+    link: authLink.concat(splitLink),
+    cache: new InMemoryCache()
+});
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,9 @@ import { RouterProvider } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./main.css";
 import router from "./routes";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+import client from './lib/apollo.cjs'
 
-const client = new ApolloClient({
-  uri: process.env.HASURA_HTTP_URL,
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

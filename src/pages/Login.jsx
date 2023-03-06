2937268d514cbb52ccd19c7f5ac1b3cc8bc5e28f
DX <dx@DXs-MacBook-Pro.local>
2023-03-06 14:08:56 +0800

    message

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,7 @@ const LoginPage = () => {
                 <CardBody minWidth="550px">
                     <Text>Username</Text> 
                     <Input placeholder='Username' type='email' required/>
-                    <Text visibility="hidden">Incorrect Username</Text>
+                    <Text visibility="hidden">Incorrect WOWwweee</Text>
                     <Text>Password</Text> 
                     <Input placeholder='Password' type='password' required/>
                     <Text visibility="hidden">Incorrect Password</Text>
---
+++ src/routes/loginLayout.jsx
@@ -0,0 +1,68 @@
+
+
+export const LoginLayout = () => {
+
+
+    return (
+
+
+
+        <div class="min-h-screen bg-gray-#2D3748 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-md">
+    <img class="mx-auto h-12 w-auto" src="..."/> 
+    <h2 class="mt-6 text-center text-3xl font-extrabold  text-black ">
+      Fleet Management
+    </h2>
+  </div>
+
+  <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+    <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+      <form class="space-y-6" action="#" method="POST">
+        <div>
+
+        <h2 class="mt-6 text-center text-3xl font-extrabold  text-black ">
+      Fleet Management
+    </h2>
+
+          <label for="email" class="block text-sm font-medium text-gray-700">
+            Email address
+          </label>
+          <div class="mt-1">
+            <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+          </div>
+        </div>
+
+        <div>
+          <label for="password" class="block text-sm font-medium text-gray-700">
+            Password
+          </label>
+          <div class="mt-1">
+            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 2a2 2 0 00-2 2v2H6a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8a2 2 0 00-2-2h-2V4a2 2 0 00-2-2h-4zm0 10a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+              </svg>
+            </span>
+            Sign in
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+    )
+   
+
+
+}

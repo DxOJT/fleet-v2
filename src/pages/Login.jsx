d11435c752b70b2c1b2b7c1cc24b40bc50ead3f9
Cloderaldo Pampanga <clod.p@freshclinics.com>
2023-03-06 16:05:57 +0800

    fix conflict

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,33 @@
-import { Box, Input, Image, Button } from '@chakra-ui/react'
+import { useState } from 'react';
+import { Box, Input, Image } from '@chakra-ui/react'
 import { Card, CardBody, Text } from '@chakra-ui/react'
 const LoginPage = () => {
 
-
+    const email = "admin@test"
+    const password = "admin"
+    const [inputEmail, setInputEmail] = useState('')
+    const [inputPassword, setInputPassword] = useState('')
+    const [emailError, setEmailError] = useState(false)
+    const [passwordError, setPasswordError] = useState(false)
     return (  
-        <Box minH="100vh" display="flex" justifyContent="center" alignItems="center" bg={"#2D3748"}>
-            <Card alignItems="center" border= "1px" borderColor="orange">
-                    <Image borderRadius="full" src='/fleet.png' boxSize="100px"/>
-                    <Text fontSize="30px">Fleet Management</Text>
+        <Box 
+            minH="100vh" 
+            display="flex" 
+            justifyContent="center" 
+            alignItems="center"
+            bg={"#2D3748"}>
+            <Card alignItems="center" borderColor="orange" borderWidth="2px">
+                    <Image src='/fleet.png' boxSize="100px"/>
+                    <Text fontSize="30px" fontWeight="bold">Fleet Management</Text>
                 <CardBody minWidth="550px">
                     <Text>Username</Text> 
-                    <Input placeholder='Username' type='email'  required/>
-                    <Text visibility="hidden">Incorrect Username</Text>
-                    <Input placeholder='Username' type='email' required/>
-                    <Text visibility="hidden">Incorrect WOWwweee</Text>
+                    <Input placeholder='Username' type='email' required value={inputEmail} isInvalid={emailError}
+                    onChange={(e) => {setInputEmail(e.target.value)}}/>
+                    <Text visibility={emailError?"visible":"hidden"} color="red">Incorrect Username</Text>
                     <Text>Password</Text> 
-                    <Input placeholder='Password' type='password' required/>
-                    <Text visibility="hidden">Incorrect Password</Text>
-                    <Button colorScheme="orange"  width= "100%">Sign Up</Button>
+                    <Input placeholder='Password' type='password' required value={inputPassword} isInvalid={passwordError}
+                    onChange={(e) => {setInputPassword(e.target.value)}}/>
+                    <Text visibility={passwordError?"visible":"hidden"} color="red">Incorrect Password</Text>
                 </CardBody>
             </Card>
         </Box>

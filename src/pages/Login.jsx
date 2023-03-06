31e481fe3c03738d25d2c109fb52baa457864e35
Reyjan <reyjancuyog26@gmail.com>
2023-03-06 15:39:31 +0800

    Add button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,21 @@
-import { Box, Input, Image } from '@chakra-ui/react'
-import { Card, CardHeader, CardBody, CardFooter, Text } from '@chakra-ui/react'
+import { Box, Input, Image, Button } from '@chakra-ui/react'
+import { Card, CardBody, Text } from '@chakra-ui/react'
 const LoginPage = () => {
+
+
     return (  
-        <Box minH="100vh" display="flex" justifyContent="center" alignItems="center">
-            <Card alignItems="center">
+        <Box minH="100vh" display="flex" justifyContent="center" alignItems="center" bg={"#2D3748"}>
+            <Card alignItems="center" border= "1px" borderColor="orange">
                     <Image borderRadius="full" src='/fleet.png' boxSize="100px"/>
                     <Text fontSize="30px">Fleet Management</Text>
                 <CardBody minWidth="550px">
                     <Text>Username</Text> 
-                    <Input placeholder='Username' type='email' required/>
+                    <Input placeholder='Username' type='email'  required/>
                     <Text visibility="hidden">Incorrect Username</Text>
                     <Text>Password</Text> 
                     <Input placeholder='Password' type='password' required/>
                     <Text visibility="hidden">Incorrect Password</Text>
+                    <Button colorScheme="orange"  width= "100%">Sign Up</Button>
                 </CardBody>
             </Card>
         </Box>

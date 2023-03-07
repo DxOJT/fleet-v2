bd2a498d7141e4935ca80af8c4ff0000d116885d
DX <dx@DXs-MacBook-Pro.local>
2023-03-07 11:10:47 +0800

    Latest Change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,13 +8,14 @@ export const Dashboard  = () =>  {
 
 
     const  cardLayout  = {
-      width: "200px",
+      width: "205px",
       height: "80px",
       bgColor: "White",
       borderRadius: "10px",
       color: "black",
-      minWidth: "120px" ,
-      flexWrap :"nowrap"
+      flexWrap :"nowrap",
+      padding : "10px"
+      
 
     }
 
@@ -23,14 +24,16 @@ export const Dashboard  = () =>  {
 
      const  cardFont= {
         fontWeight:"bold",
-        fontSize:"12px",
+        fontSize:"30px",
+    
+        
      }
 
      const sampledata = [
         {id:1, text1:0, text2:"Franchise  in One  (1) month"},
-        {id:2, text1:0, text2:"Franchise  in One  (1) month"},
-        {id:3, text1:0, text2:"Franchise  in One  (1) month"},
-        {id:4, text1:0, text2:"Franchise  in One  (1) month"},
+        {id:2, text1:0, text2:"Vehicle  Registration expiring in (1) month "},
+        {id:3, text1:0, text2:"Driving license expiring in (1) month"},
+        {id:4, text1:0, text2:"Insurance expiring in (1) month"},
 
         
      ]
@@ -44,14 +47,14 @@ return(
     
       
       
-        <Flex  className="flex justify-center   "     rowGap={"50px"}   gap={"24px"} direction={"row"}  flexWrap="wrap" > 
+        <Flex  className="flex justify-center   "        gap={"10px"} direction={"row"}  flexWrap="wrap" > 
           
             {sampledata.map((value)=>(
 
 
             <Card    style={cardLayout}  key={value.id}>
-            <Center   height={"70%"} > < Text  style={cardFont}   > {value.text1}</Text></Center>
-            <Center>  <Text  style={cardFont} > {value.text2}</Text> </Center> 
+            <Center   height={"70%"} > < Text  style={cardFont} className ={" text-9xl mb-1 "  }   > {value.text1}</Text></Center>
+            <Center>  <Text  fontSize={"10px" } fontWeight={"bold"}   p={"2px"} > {value.text2}</Text> </Center> 
             </Card>
 
 

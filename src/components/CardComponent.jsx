be054932837e10b860fd0e6879b37b724da6fe0f
zynmurel <comingues99@gmail.com>
2023-03-17 16:57:35 +0800

    text size adjustment

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -2,8 +2,8 @@ import { Box, Card,  Center, Text } from "@chakra-ui/react"
 const CardComponent = ({value}) => { 
     return(
     <Box className=" p-4 flex flex-col items-center text-center rounded-2xl bg-white drop-shadow-md"  >
-        <Text className=" font-bold text-4xl md:text-5xl mb-1">{value.text1}</Text>
-        <Text className=" font-medium text-md md:text-xl text-gray-600">{value.text2}</Text> 
+        <Text className=" font-bold text-3xl md:text-4xl mb-1">{value.text1}</Text>
+        <Text className=" font-medium text-sm md:text-lg text-gray-600">{value.text2}</Text> 
     </Box>
     )
 }

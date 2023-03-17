ecce6cf5b6c1ebea7ab0055df5e4564087374809
Reyjan <reyjancuyog26@gmail.com>
2023-03-17 17:02:15 +0800

    fix lenting

--- src/components/ComponentTable.jsx
+++ src/components/ComponentTable.jsx
@@ -1,42 +1,48 @@
-import { Box, Button, Flex ,HStack,Input,Text,Table, Thead, Tbody, Tr, Th, Td, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Input,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Link,
+} from "@chakra-ui/react";
 
-const ComponentTable = ({driverData}) => {
+const ComponentTable = ({ driverData }) => {
   return (
-    <Table variant="simple" mt={"5"} >
-    <Thead>
-      <Tr >
-        <Th fontSize={"13"}>NAME </Th>
-        <Th fontSize={"13"}>LICENSE EXPIRY DATE</Th>
-        <Th fontSize={"13"}>ACTION</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
+    <Table variant="simple" mt={"5"}>
+      <Thead>
+        <Tr>
+          <Th fontSize={"13"}>NAME </Th>
+          <Th fontSize={"13"}>LICENSE EXPIRY DATE</Th>
+          <Th fontSize={"13"}>ACTION</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {driverData.map((value, index) => {
+          return (
+            <Tr
+              _hover={{ bg: "gray.100" }}
+              key={index}
+              borderBottom="2px ridge white"
+            >
+              <Td>{value.name}</Td>
+              <Td> {value.DateOflicense}</Td>
+              <Td>
+                <Link color={"blue.500"}> {value.Action}</Link>
+              </Td>
+            </Tr>
+          );
+        })}
+      </Tbody>
+    </Table>
+  );
+};
 
-      {driverData.map((value,index)=>{
-
-  return (
-
-    
-
-      <Tr  _hover={{ bg: "gray.100"}} key={index}  borderBottom="2px ridge white">
-        <Td   >{value.name}</Td>
-        <Td  > {value.DateOflicense}</Td>
-        <Td ><Link color={"blue.500"}> {value.Action}</Link></Td>
-
-      </Tr>   
-
-     
-  )
-
-
-      })}
-
-    </Tbody>
-  </Table>
-    );
-}
- 
 export default ComponentTable;
-
- 
-
--- src/pages/DriverList.jsx
+++ src/pages/DriverList.jsx
@@ -1,47 +1,61 @@
-import { Box, Button, Flex ,HStack,Input,Text,  } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack, Input, Text } from "@chakra-ui/react";
 import { ClassNames } from "@emotion/react";
 import ComponentTable from "../components/ComponentTable";
 
-
-
-
 const DriverList = () => {
-        const driverData=[
+  const driverData = [
+    {
+      name: "Segan Abayo",
+      DateOflicense: "12/24/2023",
+      Action: "View profile",
+    },
+    {
+      name: "Segan Abayo",
+      DateOflicense: "12/24/2023",
+      Action: "View profile",
+    },
+    {
+      name: "Segan Abayo",
+      DateOflicense: "12/24/2023",
+      Action: "View profile",
+    },
+  ];
+  return (
+    <>
+      <Box mt={"5"}>
+        <HStack className="flex justify-between">
+          <Text
+            className=" font-bold"
+            fontSize={{ base: 15, md: 23 }}
+            ml={{ base: 7, md: 0 }}
+          >
+            {" "}
+            Driver List{" "}
+          </Text>
 
-        {name: "Segan Abayo",DateOflicense: "12/24/2023" ,Action:"View profile"},
-        {name: "Segan Abayo",DateOflicense: "12/24/2023" ,Action:"View profile"},
-        {name: "Segan Abayo",DateOflicense: "12/24/2023" ,Action:"View profile"},
-                        
-                        ]
-    return ( 
-        
-
-        <>
-        <Box mt={"5"}>
-        <HStack className="flex justify-between"> 
-
-
-          <Text   className=" font-bold" fontSize={{base:15 , md:23}} 
-          ml={{base:7,md:0}} >  Driver List </Text>    
-
-          <div>  
-            <Input  w={{base:20, md:60}} mr={"5"} placeholder="Search"/>
-            <Button  w={{base:20 , md:40}} mr={"5"} px="10" mt={"-2"}  borderStyle={"solid"} borderWidth= {"1px"} borderColor={"orange.500"}  color={"orange.600"} fontSize={{base:12, md:15}} >Add Drivers</Button> 
-
-        </div>
-   </HStack>
+          <div>
+            <Input w={{ base: 20, md: 60 }} mr={"5"} placeholder="Search" />
+            <Button
+              w={{ base: 20, md: 40 }}
+              mr={"5"}
+              px="10"
+              mt={"-2"}
+              borderStyle={"solid"}
+              borderWidth={"1px"}
+              borderColor={"orange.500"}
+              color={"orange.600"}
+              fontSize={{ base: 12, md: 15 }}
+            >
+              Add Drivers
+            </Button>
+          </div>
+        </HStack>
         <Box>
-  
-        <ComponentTable driverData = {driverData}/>
- 
+          <ComponentTable driverData={driverData} />
         </Box>
+      </Box>
+    </>
+  );
+};
 
-        </Box>
-
-</>
-     )
-    ;
-}
- 
-export default DriverList
-;
+export default DriverList;

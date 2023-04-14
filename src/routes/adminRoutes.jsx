edfefd0e442a241658c3a2b6c1905ac3d97725dd
zynmurel <comingues99@gmail.com>
2023-04-14 15:48:03 +0800

    add formLayout for addDriver

--- src/routes/adminRoutes.jsx
+++ src/routes/adminRoutes.jsx
@@ -1,6 +1,7 @@
 // pages
 import Dashboard from "../pages/admin/dashboard";
 import DriverList from "../pages/admin/employee/driver";
+import GatekeeperList from "../pages/admin/employee/gatekeeper";
 import AddDriver from "../pages/admin/employee/addDriver";
 
 export default [
@@ -11,6 +12,10 @@ export default [
   {
     path: "/admin/driver-list",
     element: <DriverList />,
+  },
+  {
+    path: "/admin/gatekeeper-list",
+    element: <GatekeeperList />,
   },
   {
     path: "/admin/add-driver",

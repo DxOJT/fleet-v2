621fbaf5830f3f3548a0faf2c0fdbc9839950a50
zynmurel <comingues99@gmail.com>
2023-04-14 13:31:48 +0800

    fix conflict in adminRoutes

--- src/routes/adminRoutes.jsx
+++ src/routes/adminRoutes.jsx
@@ -1,19 +1,24 @@
 // pages
-import Dashboard from '../pages/admin/dashboard';
-import AddDriver from '../pages/admin/employee/addDriver';
-import DriverList from '../pages/admin/employee/driver';
+import Dashboard from "../pages/admin/dashboard";
+import DriverList from "../pages/admin/employee/driver";
+import GatekeeperList from "../pages/admin/employee/gatekeeper";
+import AddDriver from "../pages/admin/employee/addDriver";
 
 export default [
   {
-    path: '/admin',
+    path: "/admin",
     element: <Dashboard />,
   },
   {
-    path: '/admin/driver-list',
+    path: "/admin/driver-list",
     element: <DriverList />,
   },
   {
-    path: '/admin/add-driver',
+    path: "/admin/gatekeeper-list",
+    element: <GatekeeperList />,
+  },
+  {
+    path: "/admin/add-driver",
     element: <AddDriver />,
   },
 ];

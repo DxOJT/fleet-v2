be4d56d1be1d49100a171c0dca6eb7506ab7fbc1
Clint <clint.m@freshclinics.com>
2023-05-09 14:20:52 +0800

    build: s3

--- src/pages/admin/employee/addDriver.jsx
+++ src/pages/admin/employee/addDriver.jsx
@@ -63,6 +63,7 @@ const AddDriver = () => {
             type="primary"
             ghost
             htmlType="submit"
+            loading={loading}
           >
             Save
           </Button>
